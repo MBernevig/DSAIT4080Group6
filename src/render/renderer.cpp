093854a14a0797ce9efefab6626ccb80a547fcf9
silentstormm <github.perky262@passinbox.com>
2025-02-28 16:44:33 +0100

    feat(renderer): implement phong shading

--- src/render/renderer.cpp
+++ src/render/renderer.cpp
@@ -195,7 +195,7 @@ float Renderer::bisectionAccuracy(const Ray& ray, float t0, float t1, float isoV
 
 glm::vec3 Renderer::computePhongShading(const glm::vec3& color, const volume::GradientVoxel& gradient, const glm::vec3& L, const glm::vec3& V, float ambientCoefficient, float diffuseCoefficient, float specularCoefficient, int specularPower)
 {
-    return glm::vec3(0.0f);
+    return color * (ambientCoefficient + glm::dot(gradient.dir, -L) * diffuseCoefficient + glm::pow(glm::dot(V, glm::reflect(gradient.dir, L), specularPower) * specularCoefficient))
 }
 
 // ======= TODO: IMPLEMENT ========
